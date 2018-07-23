952dd23dc7bfeee30dc767ca03f834282fd9d25c
Utkarsh Upadhyay <musically.ut@gmail.com>
2018-07-23 14:37:49 +0200

    Update API to CVX 1.0
    sum_entries -> sum.

--- hawkes/simPointProcess.py
+++ hawkes/simPointProcess.py
@@ -2,7 +2,6 @@ import cvxpy as CVX
 import numpy as np
 import matplotlib.pyplot as plt
 
-# MATLAB: see sampleHawkes.m
 
 def sampleHawkes(lambda_0, alpha_0, w, T, Nev, seed=None):
     """Generates samples from a Hawkes process with \lambda_0 and \alpha_0 until one of the following happens:
@@ -20,7 +19,6 @@ def sampleHawkes(lambda_0, alpha_0, w, T, Nev, seed=None):
 
     return tev, Tend
 
-## MATLAB: preprocessEv.m
 
 def preprocessEv(tev, T, w):
     lambda_ti = np.zeros_like(tev, dtype=float)
@@ -32,7 +30,6 @@ def preprocessEv(tev, T, w):
 
     return lambda_ti, survival
 
-## MATLAB: Hawkes_log_lik.m
 
 def Hawkes_log_lik(T, alpha_opt, lambda_opt, lambda_ti, survival, for_cvx=False):
     # The implementation has to be different for CVX and numpy versions because
@@ -42,7 +39,7 @@ def Hawkes_log_lik(T, alpha_opt, lambda_opt, lambda_ti, survival, for_cvx=False)
     L = 0
     for i in range(len(lambda_ti)):
         if for_cvx and len(lambda_ti) > 0:
-            L += CVX.sum_entries(CVX.log(lambda_opt + alpha_opt * lambda_ti[i]))
+            L += CVX.sum(CVX.log(lambda_opt + alpha_opt * lambda_ti[i]))
         else:
             L += np.sum(np.log(lambda_opt + alpha_opt * lambda_ti[i]))
 
@@ -50,7 +47,6 @@ def Hawkes_log_lik(T, alpha_opt, lambda_opt, lambda_ti, survival, for_cvx=False)
 
     return L
 
-## MATLAB: plotHawkes.m
 
 def plotHawkes(tev, l_0, alpha_0, w, T, resolution):
     tvec = np.arange(0, T, step=T / float(resolution))
@@ -76,6 +72,7 @@ def plotHawkes(tev, l_0, alpha_0, w, T, resolution):
 
 ##################################################
 
+
 # Simulation time
 T = 10
 
@@ -94,9 +91,9 @@ w = 1
 # Number of samples to take
 Nsamples = 5
 
-tev       = [ None ] * Nsamples
-Tend      = [ None ] * Nsamples
-lambda_ti = [ None ] * Nsamples
+tev       = [None] * Nsamples
+Tend      = [None] * Nsamples
+lambda_ti = [None] * Nsamples
 survival  = np.zeros(Nsamples)
 
 for i in range(Nsamples):
@@ -104,11 +101,11 @@ for i in range(Nsamples):
     lambda_ti[i], survival[i] = preprocessEv(tev[i], Tend[i], w)
 
 plotHawkes(tev, lambda_0, alpha_0, w, T, 10000.0)
-plt.ion()  # Make the plot interactive
-plt.show() # Show the plot. May not be needed in IPython
+plt.ion()   # Make the plot interactive
+plt.show()  # Show the plot. May not be needed in IPython
 
 
-## Solution using CVX
+# Solution using CVX
 
 alpha_opt = CVX.Variable() if alpha_0 > 0 else 0
 constraints = [alpha_opt >= 0] if alpha_0 > 0 else []
